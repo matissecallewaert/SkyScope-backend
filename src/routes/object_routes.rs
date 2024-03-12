3f7cdfa25cac7d71b336fae39b80a4370f290e93
nsavvide <niels.i.savvides@gmail.com>
2024-03-12 10:45:08 +0100

    :bug: Fix non-writing perm issue

--- src/routes/object_routes.rs
+++ src/routes/object_routes.rs
@@ -1,46 +1,45 @@
 use rocket::http::Status;
 use rocket::response::status::Custom;
 use rocket::serde::json::Json;
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
-use tokio::fs::{self, OpenOptions};
-use tokio::io::{AsyncBufReadExt, BufReader};
 
 #[get("/get-object-list")]
-pub async fn get_object_list() -> Result<Json<Vec<String>>, Custom<&'static str>> {
+pub fn get_object_list() -> Result<Json<Vec<String>>, Custom<&'static str>> {
     let object_list_path = Path::new("assets/object_list/object_list.txt");
 
-    if let Some(parent) = object_list_path.parent() {
-        fs::create_dir_all(parent).await.map_err(|_| {
-            Custom(
-                Status::InternalServerError,
-                "Cannot create directory for object list",
-            )
-        })?;
-    }
-
-    let file = OpenOptions::new()
+    let file = match OpenOptions::new()
         .read(true)
-        .create(true) // This option ensures the file is created if it does not exist
+        .write(true) // Necessary for creating a file
+        .create(true)
         .open(object_list_path)
-        .await
-        .map_err(|_| {
-            Custom(
+    {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Failed to open or create the file: {:?}", e);
+            return Err(Custom(
                 Status::InternalServerError,
                 "Cannot open or create object list file",
-            )
-        })?;
+            ));
+        }
+    };
 
     let reader = BufReader::new(file);
-    let mut lines = reader.lines();
-    let mut objects = Vec::new();
+    let lines = reader.lines();
 
-    while let Some(line) = lines.next_line().await.map_err(|_| {
-        Custom(
-            Status::InternalServerError,
-            "Failed to read line from object list file",
-        )
-    })? {
-        objects.push(line.to_string());
+    let mut objects = Vec::new();
+    for line in lines {
+        match line {
+            Ok(line) => objects.push(line),
+            Err(e) => {
+                eprintln!("Failed to read line: {:?}", e);
+                return Err(Custom(
+                    Status::InternalServerError,
+                    "Failed to read line from object list file",
+                ));
+            }
+        }
     }
 
     Ok(Json(objects))
