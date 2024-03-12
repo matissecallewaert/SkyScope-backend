3cdcb6379d62b9186368971574b4716e696f3ef2
nsavvide <niels.i.savvides@gmail.com>
2024-03-12 09:55:22 +0100

    :goal_net: Upgrade robustness by creating file if non-existent

--- src/routes/object_routes.rs
+++ src/routes/object_routes.rs
@@ -1,25 +1,38 @@
-use rocket::serde::json::Json;
-
-use rocket::data::{Data, ToByteUnit};
 use rocket::http::Status;
 use rocket::response::status::Custom;
-use std::fs::OpenOptions;
-use std::io::Write;
+use rocket::serde::json::Json;
 use std::path::Path;
-use tokio::fs::File;
+use tokio::fs::{self, OpenOptions};
 use tokio::io::{AsyncBufReadExt, BufReader};
 
-#[get("/object_list")]
+#[get("/get-object-list")]
 pub async fn get_object_list() -> Result<Json<Vec<String>>, Custom<&'static str>> {
     let object_list_path = Path::new("assets/object_list/object_list.txt");
-    let mut objects = Vec::new();
 
-    let file = File::open(object_list_path)
+    if let Some(parent) = object_list_path.parent() {
+        fs::create_dir_all(parent).await.map_err(|_| {
+            Custom(
+                Status::InternalServerError,
+                "Cannot create directory for object list",
+            )
+        })?;
+    }
+
+    let file = OpenOptions::new()
+        .read(true)
+        .create(true) // This option ensures the file is created if it does not exist
+        .open(object_list_path)
         .await
-        .map_err(|_| Custom(Status::InternalServerError, "Cannot open object list file"))?;
+        .map_err(|_| {
+            Custom(
+                Status::InternalServerError,
+                "Cannot open or create object list file",
+            )
+        })?;
 
     let reader = BufReader::new(file);
     let mut lines = reader.lines();
+    let mut objects = Vec::new();
 
     while let Some(line) = lines.next_line().await.map_err(|_| {
         Custom(
@@ -31,44 +44,4 @@ pub async fn get_object_list() -> Result<Json<Vec<String>>, Custom<&'static str>
     }
 
     Ok(Json(objects))
-}
-
-#[post("/add_object", data = "<object_name>")]
-pub async fn add_object_to_list(
-    object_name: Data<'_>,
-) -> Result<&'static str, Custom<&'static str>> {
-    // Define the path to the file
-    let path = Path::new("assets/object_list/object_list.txt");
-
-    // Convert Data to a String
-    let string_data = object_name.open(2.mebibytes()).into_string().await;
-
-    match string_data {
-        Ok(string) if string.is_complete() => {
-            let mut file = match OpenOptions::new()
-                .create(true) // Create file if it does not exist
-                .append(true) // Append to the file
-                .open(&path)
-            {
-                Ok(file) => file,
-                Err(_) => {
-                    return Err(Custom(
-                        rocket::http::Status::InternalServerError,
-                        "Failed to open file",
-                    ))
-                }
-            };
-
-            // Write the string to the file, appending a newline
-            if let Err(_) = writeln!(file, "{}", string.into_inner()) {
-                return Err(Custom(
-                    rocket::http::Status::InternalServerError,
-                    "Failed to write to file",
-                ));
-            }
-
-            Ok("Object Uploaded Successfully")
-        }
-        _ => Err(Custom(rocket::http::Status::BadRequest, "Invalid data")),
-    }
 }
